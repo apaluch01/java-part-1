050246688d8973b9d93183d40e27978792a3f561
Aleksandra <apaluch@griddynamics.com>
2022-07-23 18:17:21 +0200

    first implementation created in main method

--- src/ReverseStringMaker.java
+++ src/ReverseStringMaker.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReverseStringMaker {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        String stringToReverse = input.nextLine();
+
+        int length = stringToReverse.length();
+
+        for (int i = 0; i < length; i++){
+            System.out.print(stringToReverse.charAt(length - 1 - i));
+        }
+
+    }
 }
