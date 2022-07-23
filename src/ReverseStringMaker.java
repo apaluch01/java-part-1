9ef703f11638c793e1a5fdadb4c7cb3c1f22132f
Aleksandra <apaluch@griddynamics.com>
2022-07-23 18:32:48 +0200

    third implementation of task 3 added to main method

--- src/ReverseStringMaker.java
+++ src/ReverseStringMaker.java
@@ -9,7 +9,7 @@ public class ReverseStringMaker {
         int length = stringToReverse.length();
 
         for (int i = 0; i < length; i++){
-            System.out.print(stringToReverse.charAt(length - 1 - i));
+           System.out.print(stringToReverse.charAt(length - 1 - i));
         }
         System.out.println("\n");
 
@@ -18,5 +18,11 @@ public class ReverseStringMaker {
         System.out.println(reverse);
         System.out.println("\n");
 
+        String frontAddition = "";
+        for (int i = length - 1; i >= 0; i--){
+            frontAddition = frontAddition + stringToReverse.charAt(i);
+            System.out.println(frontAddition);
+        }
+        System.out.println(frontAddition);
     }
 }
