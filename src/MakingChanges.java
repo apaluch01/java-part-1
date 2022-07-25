19e9f75504f5ddad3e0d2b45c07143d1e6c755f0
Aleksandra <apaluch@griddynamics.com>
2022-07-25 14:56:35 +0200

    completed third task

--- src/MakingChanges.java
+++ src/MakingChanges.java
@@ -1,11 +1,36 @@
+import org.jetbrains.annotations.NotNull;
+
 class Person {
     String name;
     int age;
 }
 
 public class MakingChanges {
-    public static void changeIdentities(Person p1, Person p2) {
-        // write your code here
+    public static void changeIdentities(@NotNull Person p1, @NotNull Person p2) {
+        String swapName = p1.name;
+        p1.name = p2.name;
+        p2.name = swapName;
 
+        int swapAge = p1.age;
+        p1.age = p2.age;
+        p2.age = p1.age;
+    }
+
+    public static void main(String args[]){
+        Person p1 = new Person();
+        Person p2 = new Person();
+
+        p1.name = "Alyssa";
+        p2.name = "Max";
+        p1.age = 18;
+        p2.age = 23;
+
+        System.out.println(p1.name + " " + p1.age);
+        System.out.println(p2.name + " " + p2.age + "\n");
+
+        changeIdentities(p1, p2);
+
+        System.out.println(p1.name + " " + p1.age);
+        System.out.println(p2.name + " " + p2.age);
     }
 }
