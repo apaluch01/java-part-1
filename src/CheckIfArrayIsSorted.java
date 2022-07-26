7b11927ed9ba936353fe13c2656daa581be5b378
Aleksandra <apaluch@griddynamics.com>
2022-07-26 15:13:35 +0200

    renamed checker variable to isSorted

--- src/CheckIfArrayIsSorted.java
+++ src/CheckIfArrayIsSorted.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CheckIfArrayIsSorted {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean checker = true;
+        boolean isSorted = true;
         int length = input.nextInt();
         input.nextLine();
         String numbers = input.nextLine();
@@ -12,11 +12,11 @@ public class CheckIfArrayIsSorted {
 
         for (int i = 0; i < length - 1; i++){
             if (Integer.parseInt(array[i]) > Integer.parseInt(array[i + 1])){
-                checker = false;
+                isSorted = false;
                 break;
             }
         }
 
-        System.out.println(checker);
+        System.out.println(isSorted);
     }
 }
