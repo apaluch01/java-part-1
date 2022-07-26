498b88082154d8c04c8267680a789ed1a4b56dc1
Aleksandra <apaluch@griddynamics.com>
2022-07-26 17:18:14 +0200

    added constructor, getter and setter to Person class, adjusted rest of the code to them

--- src/MakingChanges.java
+++ src/MakingChanges.java
@@ -1,34 +1,52 @@
 import org.jetbrains.annotations.NotNull;
 
 class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public Person (String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
 public class MakingChanges {
     public static void changeIdentities(@NotNull Person p1, @NotNull Person p2) {
-        String swapName = p1.name;
-        p1.name = p2.name;
-        p2.name = swapName;
+        String swapName = p1.getName();
+        p1.setName(p2.getName());
+        p2.setName(swapName);
 
-        p1.age = p1.age ^ p2.age ^ (p2.age = p1.age);
+        p1.setAge(p1.getAge() ^ p2.getAge());
+        p2.setAge(p1.getAge() ^ p2.getAge());
+        p1.setAge(p1.getAge() ^ p2.getAge());
     }
 
-    public static void main(String args[]){
-        Person p1 = new Person();
-        Person p2 = new Person();
+    public static void main(String args[]) {
+        Person p1 = new Person("Alyssa", 18);
+        Person p2 = new Person("Max", 23);
 
-        p1.name = "Alyssa";
-        p2.name = "Max";
-        p1.age = 18;
-        p2.age = 23;
-
-        System.out.println(p1.name + " " + p1.age);
-        System.out.println(p2.name + " " + p2.age + "\n");
+        System.out.println(p1.getName() + " " + p1.getAge());
+        System.out.println(p2.getName() + " " + p2.getAge() + "\n");
 
         changeIdentities(p1, p2);
 
-        System.out.println(p1.name + " " + p1.age);
-        System.out.println(p2.name + " " + p2.age);
+        System.out.println(p1.getName() + " " + p1.getAge());
+        System.out.println(p2.getName() + " " + p2.getAge());
     }
 }
