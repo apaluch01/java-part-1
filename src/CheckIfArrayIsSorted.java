d0a686af070d1052d554a354a553b995306e86bd
Aleksandra <apaluch@griddynamics.com>
2022-07-27 17:25:07 +0200

    created exception handling method, added previosly missing exceptions

--- src/CheckIfArrayIsSorted.java
+++ src/CheckIfArrayIsSorted.java
@@ -20,16 +20,17 @@ class InvalidInput extends Exception {
 }
 
 public class CheckIfArrayIsSorted {
-    public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isSorted = true;
-
-        int length = input.nextInt();
-        input.nextLine();
-        String numbers = input.nextLine();
-
+    static boolean exceptionHandler(String numbers, int userLength) {
         try {
-            for (int i = 0; i < numbers.length(); i++) {
+            if (numbers.charAt(0) == 32) {
+                throw new InvalidInput("You should start input with a number");
+            }
+
+            if (numbers.charAt(numbers.length() - 1) == 32) {
+                throw new InvalidInput("You should use spaces only for separating numbers");
+            }
+
+            for (int i = 0; i < numbers.length() - 1; i++) {
                 if ((numbers.charAt(i) == 32) && (numbers.charAt(i + 1) == 32)) {
                     throw new InvalidInput("You can only input numbers and singular spaces to separate them");
                 }
@@ -47,8 +48,8 @@ public class CheckIfArrayIsSorted {
                 }
             }
 
-            if (spaceCount != length - 1) {
-                if (spaceCount < length - 1) {
+            if (spaceCount != userLength - 1) {
+                if (spaceCount < userLength - 1) {
                     throw new NotEnoughNumbers("You have to input the amount of numbers you've already provided - " +
                             "now it's not enough numbers");
                 }
@@ -60,17 +61,29 @@ public class CheckIfArrayIsSorted {
         }
         catch (InvalidInput | NotEnoughNumbers | TooManyNumbers e){
             System.out.println(e.getMessage());
+            return false;
         }
+        return true;
+    }
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isSorted = true;
 
-        String[] array = numbers.split(" ");
+        int length = input.nextInt();
+        input.nextLine();
+        String numbers = input.nextLine();
 
-        for (int i = 0; i < length - 1; i++){
-            if (Integer.parseInt(array[i]) > Integer.parseInt(array[i + 1])){
-                isSorted = false;
-                break;
+        if (exceptionHandler(numbers, length)) {
+            String[] array = numbers.split(" ");
+
+            for (int i = 0; i < length - 1; i++){
+                if (Integer.parseInt(array[i]) > Integer.parseInt(array[i + 1])){
+                    isSorted = false;
+                    break;
+                }
             }
-        }
 
-        System.out.println(isSorted);
+            System.out.println(isSorted);
+        }
     }
 }
