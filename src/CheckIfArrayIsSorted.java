39cd645de0e16409a541f685ccb485c1d87ccf66
Aleksandra <apaluch@griddynamics.com>
2022-07-27 15:47:31 +0200

    first implementation of custom exceptions for task 1

--- src/CheckIfArrayIsSorted.java
+++ src/CheckIfArrayIsSorted.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 //too many numbers compared to given length
 class TooManyNumbers extends Exception {
-
+    public TooManyNumbers(String s){
+        super(s);
+    }
 }
 //not enough numbers compared to given length
 class NotEnoughNumbers extends Exception {
-
+    public NotEnoughNumbers(String s){
+        super(s);
+    }
 }
 //invalid input
 class InvalidInput extends Exception {
-
+    public InvalidInput(String s){
+        super(s);
+    }
 }
 
 public class CheckIfArrayIsSorted {
@@ -20,7 +26,42 @@ public class CheckIfArrayIsSorted {
 
         int length = input.nextInt();
         input.nextLine();
-        String numbers = input.nextLine();
+
+        int spaceCount = 0;
+        try{
+            String numbers = input.nextLine();
+            for (int i = 0; i < numbers.length(); i++) {
+                if ((numbers.charAt(i) < 48) || (numbers.charAt(i) > 57)) {
+                    if (numbers.charAt(i) == 32) {
+                        spaceCount++;
+                    }
+                    else {
+                        throw new InvalidInput("You can only input numbers and spaces to separate them");
+                        break;
+                    }
+                }
+            }
+
+            if (spaceCount != length - 1) {
+                if (spaceCount < length - 1) {
+                    throw new NotEnoughNumbers("You have to input the amount of numbers you've already provided - " +
+                            "now it's not enough numbers");
+                }
+                else {
+                    throw new TooManyNumbers("You have to input the amount of numbers you've already provided - " +
+                            "now it's too many numbers");
+                }
+            }
+        }
+        catch (InvalidInput e){
+            System.out.println(e.getMessage());
+        }
+        catch (TooManyNumbers e){
+            System.out.println(e.getMessage());
+        }
+        catch (NotEnoughNumbers e){
+            System.out.println(e.getMessage());
+        }
 
         String[] array = numbers.split(" ");
 
