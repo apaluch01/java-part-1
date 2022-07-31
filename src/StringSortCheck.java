010a38fb794a050a5553409427813b26c45f7bae
Aleksandra <apaluch@griddynamics.com>
2022-08-01 01:08:08 +0200

    changed custom exceptions to RuntimeExceptions

--- src/StringSortCheck.java
+++ src/StringSortCheck.java
@@ -1,69 +1,62 @@
 import java.util.Scanner;
 
 //too many numbers compared to given length
-class TooManyNumbers extends Exception {
+class TooManyNumbers extends RuntimeException {
     public TooManyNumbers(String s){
         super(s);
     }
 }
 //not enough numbers compared to given length
-class NotEnoughNumbers extends Exception {
+class NotEnoughNumbers extends RuntimeException {
     public NotEnoughNumbers(String s){
         super(s);
     }
 }
 //invalid input
-class InvalidInput extends Exception {
+class InvalidInput extends RuntimeException {
     public InvalidInput(String s){
         super(s);
     }
 }
 
 public class StringSortCheck {
-    static boolean inputExceptionHandling(String numbers, int userLength) {
-        try {
-            if (numbers.charAt(0) == 32) {
-                throw new InvalidInput("You should start input with a number");
-            }
+    static void inputExceptionHandling(String numbers, int userLength) throws RuntimeException{
+        if (numbers.charAt(0) == 32) {
+            throw new InvalidInput("You should start input with a number");
+        }
 
-            if (numbers.charAt(numbers.length() - 1) == 32) {
-                throw new InvalidInput("You should use spaces only for separating numbers");
-            }
+        if (numbers.charAt(numbers.length() - 1) == 32) {
+            throw new InvalidInput("You should use spaces only for separating numbers");
+        }
 
-            for (int i = 0; i < numbers.length() - 1; i++) {
-                if ((numbers.charAt(i) == 32) && (numbers.charAt(i + 1) == 32)) {
-                    throw new InvalidInput("You can only input numbers and singular spaces to separate them");
-                }
-            }
-
-            int spaceCount = 0;
-            for (int i = 0; i < numbers.length(); i++) {
-                if (((numbers.charAt(i) >=  48) && (numbers.charAt(i) <= 57)) || numbers.charAt(i) == 32) {
-                    if (numbers.charAt(i) == 32) {
-                        spaceCount++;
-                    }
-                }
-                else {
-                    throw new InvalidInput("You can only input numbers and singular spaces to separate them");
-                }
-            }
-
-            if (spaceCount != userLength - 1) {
-                if (spaceCount < userLength - 1) {
-                    throw new NotEnoughNumbers("You have to input the amount of numbers you've already provided - " +
-                            "now it's not enough numbers");
-                }
-                else {
-                    throw new TooManyNumbers("You have to input the amount of numbers you've already provided - " +
-                            "now it's too many numbers");
-                }
+        for (int i = 0; i < numbers.length() - 1; i++) {
+            if ((numbers.charAt(i) == 32) && (numbers.charAt(i + 1) == 32)) {
+                throw new InvalidInput("You can only input numbers and singular spaces to separate them");
             }
         }
-        catch (InvalidInput | NotEnoughNumbers | TooManyNumbers e){
-            System.out.println(e.getMessage());
-            return false;
+
+        int spaceCount = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (((numbers.charAt(i) >=  48) && (numbers.charAt(i) <= 57)) || numbers.charAt(i) == 32) {
+                if (numbers.charAt(i) == 32) {
+                    spaceCount++;
+                }
+            }
+            else {
+                throw new InvalidInput("You can only input numbers and singular spaces to separate them");
+            }
         }
-        return true;
+
+        if (spaceCount != userLength - 1) {
+            if (spaceCount < userLength - 1) {
+                throw new NotEnoughNumbers("You have to input the amount of numbers you've already provided - " +
+                        "now it's not enough numbers");
+            }
+            else {
+                throw new TooManyNumbers("You have to input the amount of numbers you've already provided - " +
+                        "now it's too many numbers");
+            }
+        }
     }
 
     static boolean CheckingIfArrayIsSorted (String[] array, int length) {
@@ -85,9 +78,13 @@ public class StringSortCheck {
         input.nextLine();
         String numbers = input.nextLine();
 
-        if (inputExceptionHandling(numbers, length)) {
+        try {
+            inputExceptionHandling(numbers, length);
             String[] array = numbers.split(" ");
             System.out.println(CheckingIfArrayIsSorted(array, length));
+        }
+        catch (RuntimeException e){
+            System.out.println(e.getMessage());
         }
     }
 }
