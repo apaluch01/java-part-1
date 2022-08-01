efc96945600e529a5627d86b6551b5a7cda91955
Aleksandra <apaluch@griddynamics.com>
2022-08-01 02:02:24 +0200

    space change

--- src/ToReverseMethods.java
+++ src/ToReverseMethods.java
@@ -18,7 +18,7 @@ public class ToReverseMethods{
     public String charAdditionReverse (String toReverse){
         String frontAddition = "";
         int length = toReverse.length();
-        
+
         for (int i = length - 1; i >= 0; i--){
             frontAddition = frontAddition + toReverse.charAt(i);
         }
