3c600c208fb6a744be2a6277da5731a64321ee1f
apaluch <apaluch@griddynamics.com>
2022-08-02 11:31:39 +0200

    moved user and account creating to data provider

--- src/AccountManagerTest.java
+++ src/AccountManagerTest.java
@@ -4,9 +4,8 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import javax.xml.validation.Validator;
-import java.util.Arrays;
-import java.util.Collection;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @RunWith(Enclosed.class)
 public class AccountManagerTest {
@@ -15,37 +14,48 @@ public class AccountManagerTest {
         @Parameterized.Parameters
         public static Collection<Object[]> getBalance() {
             return Arrays.asList(new Object[][]{
-                    {0, 3},
-                    {199999, 3},
-                    {200000, 2},
-                    {249999, 2},
-                    {250000, 1},
-                    {299999, 1},
-                    {300000, 0},
+                    {0, 3, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
+                    {199999, 3, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
+                    {200000, 2, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
+                    {249999, 2, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
+                    {250000, 1, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
+                    {299999, 1, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
+                    {300000, 0, Arrays.asList(new Account(1, 200000, new User(1, "John",
+                            "Doe")), new Account(2, 250000, new User(2, "Max",
+                            "Maximoff")), new Account(3, 300000, new User(3, "Jonathan", "Cage")))},
             });
         }
 
-        private int balance;
+        private int balanceToCheck;
         private int expectedCount;
+        private Account[] accounts;
 
-        public TheParameterizedTests(int balance, int expectedCount) {
-            this.balance = balance;
+        public TheParameterizedTests(int balanceToCheck, int expectedCount, List<Account> accountsList) {
+            this.balanceToCheck = balanceToCheck;
             this.expectedCount = expectedCount;
+            this.accounts = new Account[accountsList.size()];
+            for (int i = 0; i < accountsList.size(); i++){
+                this.accounts[i] = accountsList.get(i);
+            }
         }
 
         @Test
         public void shouldCountAccountsWithBalanceGreaterThen () {
-            User us1 = new User(10, "John", "Smith");
-            User us2 = new User(1, "Max", "Maximoff");
-            User us3 = new User(2, "Jonathan", "Cage");
-            Account acc1 = new Account(1, 250000, us1);
-            Account acc2 = new Account(2, 300000, us2);
-            Account acc3 = new Account(3, 200000, us3);
-
-            Account[] accounts = new Account[]{acc1, acc2, acc3};
             AccountService service = new AccountServiceImpl(accounts);
 
-            int actualCount = (int) service.countAccountsWithBalanceGreaterThan(balance);
+            int actualCount = (int) service.countAccountsWithBalanceGreaterThan(balanceToCheck);
             Assert.assertEquals(actualCount, expectedCount);
         }
     }
